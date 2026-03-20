0ae6ba7d5cd0883e15363b51849373c3cd780ec1
Tvoja Karma <Tvoja_Karma@mail.ru>
2026-03-20 12:27:17 +0300

    Init front commit

---
+++ vite.config.js
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      devOptions: {
+        enabled: true,           // Включаем PWA в режиме разработки
+        type: 'module',           // Тип сервис-воркера
+        navigateFallback: 'index.html'
+      },
+      manifest: {
+        name: "EduHub",
+        short_name: "EduHub",
+        start_url: "/",
+        display: "standalone",  // критично важно!
+        theme_color: "#ebff90",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+
+  ],
+  server: {
+    // Разрешаем все хосты(для теста), так как они вечно меняются
+    allowedHosts: true
+  }
+})
+
+
