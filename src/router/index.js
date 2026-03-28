1903d62e9a85acb76e9e40956a63653a2223fcdb
Tvoja Karma <Tvoja_Karma@mail.ru>
2026-03-28 12:38:09 +0300

    Added register page

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '../pages/LoginPage.vue'
-// пока создадим заглушки для остальных страниц
 import SubjectsPage from '../pages/SubjectsPage.vue'
+import RegisterPage from '../pages/RegisterPage.vue'
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: LoginPage },
   { path: '/subjects', component: SubjectsPage },
+  { path: '/register', component: RegisterPage },
   // ... другие
 ]
 
