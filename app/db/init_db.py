03b6f2063413354fb19391439a448b6359913a6c
alex <romankevichalexey@gmail.com>
2026-04-15 21:57:31 +0300

    clear

--- app/db/init_db.py
+++
@@ -1,8 +0,0 @@
-from app.db.database import engine
-from app.models import Base
-
-def create_tables():
-    Base.metadata.create_all(bind=engine)
-
-if __name__ == "__main__":
-    create_tables()
--- run.sh
+++
@@ -1,1 +0,0 @@
-uv run uvicorn app.main:app --port 10000 --host 0.0.0.0
